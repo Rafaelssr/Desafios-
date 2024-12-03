84e04a37231468efa388523f5f4ec300c80189b4
Rafael Soares <rafael.soares@amigotech.com.br>
2024-12-03 18:23:19 -0300

    commit inicial em uma nova branch

---
+++ desafio3-md-antigo/dashboard.js
@@ -0,0 +1,28 @@
+// - Suponhamos que você é gestor de uma clínica e deseja obter alguns dados para ter noção da movimentação e execução de procedimentos que estão sendo executados e se os mesmos estão gerando valor considerável.
+// - Você quer saber se o seu faturamento está indo bem!
+
+// - Quantidade de procedimentos agrupado por ID
+// - Quantidade de procedimentos por group_key
+// - Quantidade de procedimentos por attendance_id
+// - Quantidade de procedimentos por finance_id
+// - Total produzido (price), total liquido (liquid_price), total recebido (received_value) e total não recebido (liquid_price - received_value) por procedure_id
+// - Totais por tiss_type (tipo de guia)
+// - Agrupar procedimentos por atendimento
+// - Agrupar procedimentos por financeiro
+// - Totais por data (dia / mes / ano)
+
+import data from "./jsonDashboard"; 
+
+const fetchPoints = async () => {
+  try {
+      const guideResponse = await fetch("./jsonDashboard.js");
+      const data = await guideResponse.json();
+      console.log(data);
+  } catch (error) {
+    
+    console.error("error", error);
+  }
+ 
+};
+
+fetchPoints();
